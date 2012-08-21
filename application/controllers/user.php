97435c58958d2e6f48fdb96c8d1ba8cb081a379a
Davor Leipold <dleipold@carnet.hr>
2012-08-21 18:47:17 +0200

    prijava

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -7,18 +7,27 @@ class User extends CI_Controller {
 	}
 
 	public function podaci() {
-		$this->load->model("user/userprofile");
-		$podaci = $this->userprofile;
-		
-		//podaci za trenutno logiranu osobu
-		$user_id = $this->session->userdata['user_id'];
-		$podaci = $podaci->getUserById($user_id);
-		$data = array("podaci" =>  $podaci[0]);
-		$this->layout->view('user/podaci', $data);
+		if (!$this->tank_auth->is_logged_in()) {							// not logged in or activated
+			redirect('/auth/login/');
+		} else {
+			$this->load->model("user/userprofile");
+			$podaci = $this->userprofile;
+			
+			//podaci za trenutno logiranu osobu
+			$user_id = $this->session->userdata['user_id'];
+			$podaci = $podaci->getUserById($user_id);
+			$data = array("podaci" =>  $podaci[0]);
+			$this->layout->view('user/podaci', $data);
+		}
 	}
 
 	public function prijave() {
-		$this->layout->view('user/prijave');
+		if (!$this->tank_auth->is_logged_in()) {							// not logged in or activated
+			redirect('/auth/login/');
+
+		} else {
+			$this->layout->view('user/prijave');
+		}
 	}
 	public function registracija() {
 		$this->layout->view('user/registracija');
